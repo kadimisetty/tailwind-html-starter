40c34b09d1cc679eff58dc6fcc1faa5c22a2872c
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2022-01-28 12:03:28 +0530

    setup inter as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ["./public/index.html"],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
